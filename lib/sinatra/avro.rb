4dc4af256739706b4c79acbfda5338bdc9487aa3
Daniel Schierbeck <dasch@zendesk.com>
2015-02-27 14:11:48 -0800

    Generate Gem scaffolding

---
+++ lib/sinatra/avro.rb
@@ -0,0 +1,7 @@
+require "sinatra/avro/version"
+
+module Sinatra
+  module Avro
+    # Your code goes here...
+  end
+end
---
+++ lib/sinatra/avro/version.rb
@@ -0,0 +1,5 @@
+module Sinatra
+  module Avro
+    VERSION = "0.0.1"
+  end
+end
